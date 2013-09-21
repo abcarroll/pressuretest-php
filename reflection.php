d5fafaa5957e64da9976b1eec4442789bc4e4e80
A.B. Carroll <ben@hl9.net>
2013-09-20 22:26:14 -0500

    Fixed blank parm name issue + escaping issue with $f in circ ref create

--- reflection.php
+++ reflection.php
@@ -5,18 +5,19 @@
 		$r = new ReflectionFunction($f);
 		$p = $r->getParameters();
 
-		echo "\t\t\t\t\t\$$f = [$f(";
+		echo "\t\t\t\$$f = [$f(";
 		$z = '';
 		foreach($p as $i) {
-			if($i->getName() == '...') continue;
+			$pName = $i->getName();
+			if($pName == '...' || empty($pName)) continue;
 			$z .= '$'.$i->getName();
-			if($i->isDefaultValueAvailable()) { 
+			if($i->isDefaultValueAvailable()) { 				
 				$z .= ' = ' . $i->getDefaultValue();
 			}
 
 			$z .= ', ';
 		}
 		$z = substr($z, 0, -2);
-		echo $z . ")]; \$f = [\$$f];\n";
+		echo $z . ")]; \$$f = [\$$f];\n";
 
 	}
